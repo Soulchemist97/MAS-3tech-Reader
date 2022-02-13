1b2a623c585482709bc704702fb23a2d94e0357e
Soulchemist97 <Soulchemist97@users.noreply.github.com>
2022-02-13 22:21:29 +0100

    GUI mit grundlegender Funktion

--- MAS_GUI.py
+++ MAS_GUI.py
@@ -18,12 +18,11 @@ Excel_Frage = "n"
 PDF_Frage = "n"
 
 
-def Indexing():
-    Input_dir = ui_window.PathBox.toPlainText()
+Dict_Bool = {True: "y", False:"n"}
 
-    # Aufstellorte Indizieren
-    Orte = os.listdir(Input_dir)
-    Locations=[Aufstellort(Pfad=os.path.join(Input_dir,Ort)) for Ort in Orte]
+
+def Indexing():
+
 
     if Print_Frage == "y":
         for Loc in Locations:
@@ -35,46 +34,54 @@ def Indexing():
         # Location.Rechnungen
 
         if PDF_Frage == "y":
-            Location.pdf(cut="Y")
+            Location.pdf(cut=True)
 
         if Remove_Frage == "y" or Remove_Frage == "c": 
          Location.Verschieben(remove=Remove_Frage)
-        if Excel_Frage == "y":
-            Location.Excel()
+
 
 
 
 def Start_Button_Clicked():
+    """
+    Wird ausgeführt, wenn <START> gedrückt wird.
+    """
 
     print(ui_window.PathBox.toPlainText())
 
-    if ui_window.PrintBox.isChecked():
-        Print_Frage ="y"
+    Input_dir = ui_window.PathBox.toPlainText()
 
-    if ui_window.ExcelBox.isChecked():
-        Excel_Frage = "y"
-        print("Excel Converter")
+    # Aufstellorte Indizieren
+    Orte = os.listdir(Input_dir)
+    Locations=[Aufstellort(Pfad=os.path.join(Input_dir,Ort)) for Ort in Orte]
 
-    if ui_window.PDFBox.isChecked():
-        Remove_Frage = "y"
-        print("Convert to PDF")
-    
 
-    if ui_window.IndexButton.isChecked():
-        Remove_Frage = "n"
-        print("Indizieren")
-    elif ui_window.CopyButton.isChecked():
-        Remove_Frage = "c"
-        print("Kopieren")
-    elif ui_window.RemoveButton.isChecked():
-        Remove_Frage = "y"
-        print("Verschieben")
+    for Location in Locations:
 
-    Indexing()
+        if ui_window.PrintBox.isChecked():
+            print(Location)
+
+        if ui_window.ExcelBox.isChecked():
+            Location.Excel()
+            print("Convert to Excel")
+
+        if ui_window.PDFBox.isChecked():
+            print("Convert to PDF")
+            Location.pdf(cut=True,N_Zeilen=100)
+        
+
+        if ui_window.IndexButton.isChecked():
+            print("Datei werden Indiziert")
+            Location.Verschieben(remove="n")
+        elif ui_window.CopyButton.isChecked():
+            print("Kopieren der Dateien")
+            Location.Verschieben(remove="c")
+        elif ui_window.RemoveButton.isChecked():
+            print(f"Verschieben der Dateien in {Output_dir}")
+            Location.Verschieben(remove="y")
 
 
 ui_window.StartButton.clicked.connect(Start_Button_Clicked)
-
 
 
 if __name__ =="__main__":
